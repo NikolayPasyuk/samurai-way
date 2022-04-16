0b74e17d938784f3545054645c6c34ef77942bf2
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 15:52:01 +0300

    setting up the axios create

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,11 +3,14 @@ import axios from 'axios';
 const baseUrl = 'https://social-network.samuraijs.com/api/1.0/'
 
 const instance = axios.create({
-
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'd0317f6a-b584-4afe-8a81-d7b60d153a6b'
+    }
 })
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {withCredentials: true})
+    return instance.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data)
 }
 
