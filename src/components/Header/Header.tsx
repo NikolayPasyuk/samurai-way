ae24aa6c0e924de3dd79888536d33ac3d16ead00
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 11:34:23 +0300

    added NavLink in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Header.module.css'
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
     return <header className={s.header}>
@@ -7,7 +8,7 @@ const Header = () => {
             src="https://images.fastcompany.net/image/upload/w_1280,f_auto,q_auto,fl_lossy/w_596,c_limit,q_auto:best,f_auto/fc/3034007-inline-i-applelogo.jpg"/>
 
         <div className={s.loginBlock}>
-
+            <NavLink to={'/login'}>Login</NavLink>
         </div>
     </header>
 }
