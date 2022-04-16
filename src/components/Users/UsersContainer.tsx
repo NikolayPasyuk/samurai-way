7e768f57e4dcb4027647149650f8c5e35f114e21
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 15:20:28 +0300

    added function getUsers

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -13,6 +13,7 @@ import {
 import axios from 'axios';
 import {Users} from './Users';
 import {Preloader} from '../common/preloader/Preloader';
+import {getUsers} from '../../api/api';
 
 type mapStatePropsType = {
     users: Array<UsersType>
@@ -36,8 +37,8 @@ export type UsersPropsType = mapStatePropsType & mapDispatchPropsType
 class UsersContainer extends React.Component<UsersPropsType> {
     componentDidMount() {
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true})
-            .then(response => {
+
+        getUsers().then(response => {
                 this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items)
                 this.props.setTotalUsersCount(response.data.totalCount)
