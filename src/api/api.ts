741c40278510da48016c0b7f17853672b7393aca
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 16:00:57 +0300

    create a helper object

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,6 +8,14 @@ const instance = axios.create({
     }
 })
 
+export const userAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data)
+    }
+}
+
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
     return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data)
