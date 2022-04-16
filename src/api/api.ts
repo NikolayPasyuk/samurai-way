7a4daa00ec64ff6004c64bb385cadaf72ed3f882
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 15:56:51 +0300

    added baseURL in instance

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
-const baseUrl = 'https://social-network.samuraijs.com/api/1.0/'
-
 const instance = axios.create({
     withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
         'API-KEY': 'd0317f6a-b584-4afe-8a81-d7b60d153a6b'
     }
 })
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return instance.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`)
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data)
 }
 
 export const getUsers2 = (currentPage = 1, pageSize = 10) => {
-    return instance.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`)
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data)
 }
