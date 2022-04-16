67662c995641807a8920cdf56d517137c9732bf1
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 15:48:14 +0300

    added axios.create

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,17 @@
 import axios from 'axios';
 
+const baseUrl = 'https://social-network.samuraijs.com/api/1.0/'
+
+const instance = axios.create({
+
+})
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {withCredentials: true})
+    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {withCredentials: true})
+        .then(response => response.data)
+}
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {withCredentials: true})
         .then(response => response.data)
 }
