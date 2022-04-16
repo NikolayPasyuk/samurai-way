aa33830feac4661ae70c1a390e0e4d35ad362261
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 15:28:26 +0300

    added function getUsers for onPageChanged

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -49,11 +49,10 @@ class UsersContainer extends React.Component<UsersPropsType> {
         this.props.setCurrentPage(pageNumber)
         this.props.toggleIsFetching(true)
 
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true})
-            .then(response => {
-                this.props.toggleIsFetching(false)
-                this.props.setUsers(response.data.items)
-            })
+        getUsers(pageNumber, this.props.pageSize).then(response => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(response.data.items)
+        })
     }
 
     render() {
